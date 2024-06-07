8334c4ccfe0df49824093cbba7545460dd39a7b8
deast <deast@google.com>
2024-06-07 20:14:34 +0000

    svg not png

--- vite-vanilla/idx-template.nix
+++ vite-vanilla/idx-template.nix
@@ -6,7 +6,7 @@
     mkdir -p "$WS_NAME"
     npm create -y vite@latest "$WS_NAME" -- --template ${if language == "ts" then "vanilla-ts" else "vanilla"}
     mkdir -p "$WS_NAME/.idx/"
-    cp -rf ${./icon.png} "$WS_NAME/.idx/icon.png"
+    cp -rf ${./icon.png} "$WS_NAME/.idx/icon.svg"
     cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
