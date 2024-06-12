5c0a3bed183f25544eb4e5f783c79a581fe44183
deast <deast@google.com>
2024-06-12 21:14:53 +0000

    readme

--- dart-static-server/idx-template.nix
+++ dart-static-server/idx-template.nix
@@ -14,7 +14,7 @@ rm -rf /home/user/idx/dart-static-server-test && idx-template \
     mkdir "$out"
     mkdir -p "$out/.idx/"
     cp -rf ${./dev.nix} "$out/.idx/dev.nix"
+    shopt -s dotglob; cp -r ${./dev}/* "$out"
     chmod -R +w "$out"
-    cp -r ${./dev} "$out"
   '';
 }
