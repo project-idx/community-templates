11e942dc28924950d3edccac63095566cfa7f0c8
deast <deast@google.com>
2024-07-28 16:54:48 +0000

    add blank app

--- dataconnect/idx-template.nix
+++ dataconnect/idx-template.nix
@@ -6,16 +6,19 @@ idx-template \
   --output-dir /home/user/community-templates/template-test -a '{}'
 
 */
-{pkgs, ... }: {
+{pkgs, sample ? "nextjs-email-app", ... }: {
   packages = [];
 
   bootstrap = ''
     mkdir "$out"
     mkdir "$out"/.idx
-    cp -r ${./nextjs-email-app}/* "$out"
-    cp ${./nextjs-email-app}/.firebaserc "$out"/.firebaserc
-    cp ${./nextjs-email-app}/.graphqlrc.yaml "$out"/.graphqlrc.yaml
     cp ${./dev.nix} "$out"/.idx/dev.nix
+    ${
+      if sample == "nextjs-email-app" then "cp -r ${./nextjs-email-app}/* "$out""
+      else "cp -r ${./nextjs-blank}/* "$out""
+    }
+    cp ${./firebaserc} "$out"/.firebaserc
+    cp ${./graphqlrc.yaml} "$out"/.graphqlrc.yaml
     chmod -R u+w "$out"
   '';
 }
