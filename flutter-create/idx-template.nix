33a1f3d153789f388daecf8143210a93128834e2
Rody Davis <rody.davis.jr@gmail.com>
2024-08-05 14:43:30 -0700

    Update idx-template.nix

--- flutter-create/idx-template.nix
+++ flutter-create/idx-template.nix
@@ -1,4 +1,4 @@
-{pkgs, sample ? "none", template ? "app", blank ? false, platforms ? "web,android", ...}: let
+{pkgs, ...}: let
   flutter = pkgs.fetchzip {
     url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.3-stable.tar.xz";
     hash = "sha256-j+Jl8Q8Qaj9oJVZ1LNuTuIVxX7ba0f8w38OdS/vvqtA=";
@@ -14,7 +14,7 @@
     bootstrap = ''
         cp -rf ${flutter} flutter
         chmod -R u+w flutter
-        PUB_CACHE=/tmp/pub-cache ./flutter/bin/flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
+        PUB_CACHE=/tmp/pub-cache ./flutter/bin/flutter create "$out"
         mkdir -p "$out"/.{flutter-sdk,idx}
         mv flutter "$out/.flutter-sdk/flutter"
 
