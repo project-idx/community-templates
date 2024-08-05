adb6ca13cadf170de3328121f24b8664551328f7
Rody Davis <rody.davis.jr@gmail.com>
2024-08-05 15:18:26 -0700

    Update idx-template.nix

--- flutter-create/idx-template.nix
+++ flutter-create/idx-template.nix
@@ -8,7 +8,7 @@
         pkgs.flutter
     ];
     bootstrap = ''
-        PUB_CACHE=/tmp/pub-cache flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
+        flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
         mkdir "$out"/.idx
         cp ${./dev.nix} "$out"/.idx/dev.nix
         install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
