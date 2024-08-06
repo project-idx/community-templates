26b622a79df8d994bfbc4b24128664fec70c460e
Rody Davis <rody.davis.jr@gmail.com>
2024-08-06 13:33:04 -0700

    Update idx-template.nix

--- flutter-create/idx-template.nix
+++ flutter-create/idx-template.nix
@@ -1,4 +1,9 @@
-{pkgs, sample ? "none", template ? "app", blank ? false, platforms ? "web,android", ...}: {
+{pkgs, sample ? "none", template ? "app", blank ? false, platforms ? "web,android", builtins, ...}: let 
+  flutter = builtins.fetchGit {
+    url = "https://github.com/flutter/flutter.git";
+    ref = "stable";
+  };
+  in {
     packages = [
         pkgs.curl
         pkgs.gnutar
@@ -8,8 +13,12 @@
         pkgs.flutter
     ];
     bootstrap = ''
-        flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
-        mkdir "$out"/.idx
+        cp -rf ${flutter} flutter
+        chmod -R u+w flutter
+        PUB_CACHE=/tmp/pub-cache flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
+        mkdir -p "$out"/.{flutter-sdk,idx}
+        mv flutter "$out/.flutter-sdk/flutter"
+        echo ".flutter-sdk/flutter" >> "$out/.gitignore"
         cp ${./dev.nix} "$out"/.idx/dev.nix
         install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
     '';
