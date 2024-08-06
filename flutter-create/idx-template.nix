ae3468aadf28620d0d86e2c64da9886c5e3b19fa
Rody Davis <rody.davis.jr@gmail.com>
2024-08-06 14:04:16 -0700

    Update idx-template.nix

--- flutter-create/idx-template.nix
+++ flutter-create/idx-template.nix
@@ -8,6 +8,7 @@
     ];
     bootstrap = ''
         git clone -b stable https://github.com/flutter/flutter.git
+        rm -rf ./flutter/bin/cache
         chmod -R u+w flutter
         PUB_CACHE=/tmp/pub-cache ./flutter/bin/flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
         mkdir -p "$out"/.{flutter-sdk,idx}
