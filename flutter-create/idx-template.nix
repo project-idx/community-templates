3b054ab829952f5bfc0ea09e7c5ae971fd10992b
Rody Davis <rody.davis.jr@gmail.com>
2024-08-06 14:32:03 -0700

    reverting

--- flutter-create/idx-template.nix
+++ flutter-create/idx-template.nix
@@ -5,17 +5,11 @@
         pkgs.xz
         pkgs.git
         pkgs.busybox
+        pkgs.flutter
     ];
     bootstrap = ''
-        git clone --depth 1 -b stable https://github.com/flutter/flutter.git
-        yes | ./flutter/bin/flutter doctor --android-licenses
-        ./flutter/bin/flutter doctor
-        chown -R root:root flutter
-        chmod -R u+w flutter
-        PUB_CACHE=/tmp/pub-cache ./flutter/bin/flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
-        mkdir -p "$out"/.{flutter-sdk,idx}
-        mv flutter "$out/.flutter-sdk/flutter"
-        echo ".flutter-sdk/flutter" >> "$out/.gitignore"
+        flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
+        mkdir "$out"/.idx
         cp ${./dev.nix} "$out"/.idx/dev.nix
         install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
     '';
