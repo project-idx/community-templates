301a363967427e9028f2c6c2e244f0a6fbc8a7b1
Maneesh Tewani <mtewani@google.com>
2024-08-26 20:29:40 -0700

    Removed references to email app

--- dataconnect/dart_movie_app/idx-template.nix
+++ dataconnect/dart_movie_app/idx-template.nix
@@ -6,17 +6,13 @@ idx-template \
   --output-dir /home/user/community-templates/template-test -a '{}'
 
 */
-{pkgs, sample ? "nextjs-email-app", ... }: {
+{pkgs,  ... }: {
   packages = [];
 
   bootstrap = ''
     mkdir "$out"
     mkdir "$out"/.idx
     cp ${./dev.nix} "$out"/.idx/dev.nix
-    ${
-      if sample == "nextjs-email-app" then "cp -r ${./nextjs-email-app}/* \"$out\""
-      else "cp -r ${./nextjs-blank}/* \"$out\""
-    }
     cp ${./.firebaserc} "$out"/.firebaserc
     cp ${./.graphqlrc.yaml} "$out"/.graphqlrc.yaml
     mkdir "$out"/.vscode
