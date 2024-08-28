372c1417b24a48e3378177f2287f2bc6c3170c25
Rody Davis <rody.davis.jr@gmail.com>
2024-08-28 16:41:27 -0700

    Update idx-template.nix

--- nativescript/idx-template.nix
+++ nativescript/idx-template.nix
@@ -26,9 +26,9 @@
     mkdir "$out"
     mkdir -p "$out/.idx/"
     cp -rf ${./dev.nix} "$out/.idx/dev.nix"
+    shopt -s dotglob; cp -r ${./dev}/* "$out"
     npm install nativescript
-    ./node_modules/nativescript/bin/ns create "$WS_NAME" --${template} ${if ts then "--ts" else ""} --path ${./dev}
-    shopt -s dotglob; cp -r "${./dev}/$WS_NAME/*" "$out"
+    ./node_modules/nativescript/bin/ns create example --${template} ${if ts then "--ts" else ""} --path "$out"
     chmod -R +w "$out"
     cd "$out"; npm install -D nativescript
     cd "$out"; npm install --package-lock-only --ignore-scripts
