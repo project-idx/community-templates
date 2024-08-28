a21cdb3683b469931cc57843d6d0b2780fe0201e
Rody Davis <rody.davis.jr@gmail.com>
2024-08-27 17:07:54 -0700

    revert flutter

--- flutter-create/dev.nix
+++ flutter-create/dev.nix
@@ -8,6 +8,7 @@
     pkgs.nodePackages.firebase-tools
     pkgs.jdk17
     pkgs.unzip
+    pkgs.flutter
   ];
   # Sets environment variables in the workspace
   env = {};
--- flutter-create/idx-template.nix
+++ flutter-create/idx-template.nix
@@ -5,6 +5,7 @@
         pkgs.xz
         pkgs.git
         pkgs.busybox
+        pkgs.flutter
     ];
     bootstrap = ''
         flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
