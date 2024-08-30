336d7eda5b9baad73827d56a82818e3a9042950a
Maneesh Tewani <mtewani@google.com>
2024-08-30 10:18:41 -0700

    Reverted to newer version of nixpkgs

--- dataconnect/dart/dev.nix
+++ dataconnect/dart/dev.nix
@@ -9,7 +9,7 @@ processes = {
     };
 
   # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
+  channel = "stable-24.05"; # or "unstable"
  
   
   # Use https://search.nixos.org/packages to find packages
