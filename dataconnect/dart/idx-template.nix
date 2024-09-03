10874a8bb7bb771f438465ed9070f660cc2ce655
Maneesh Tewani <mtewani@google.com>
2024-09-03 10:10:36 -0700

    Updated version of flutter

--- dataconnect/dart/idx-template.nix
+++ dataconnect/dart/idx-template.nix
@@ -18,8 +18,8 @@ idx-template \
   ];
 
   bootstrap = let flutter = pkgs.fetchzip {
-    url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.3-stable.tar.xz";
-    hash = "sha256-j+Jl8Q8Qaj9oJVZ1LNuTuIVxX7ba0f8w38OdS/vvqtA=";
+    url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.1-stable.tar.xz";
+    hash = "";
   };  in ''
   cp -rf ${flutter} flutter
   chmod -R u+w flutter
@@ -28,8 +28,6 @@ idx-template \
     echo ".flutter-sdk/flutter" >> "$out/.gitignore"
     install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
     initdb -D "$out"/local
-    printenv >> "$out"/.env
-    cp ${./dev.nix} "$out"/.idx/dev.nix
     cp ${./.firebaserc} "$out"/.firebaserc
     cp ${./.graphqlrc.yaml} "$out"/.graphqlrc.yaml
     mkdir "$out"/.vscode
