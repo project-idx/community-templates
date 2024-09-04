13a4c6a66167329cdc964077aec0bbc77a859119
Maneesh Tewani <mtewani@google.com>
2024-09-04 08:59:34 -0700

    Changed emulator installation

--- dataconnect/dart/dev.nix
+++ dataconnect/dart/dev.nix
@@ -78,7 +78,7 @@ processes = {
           manager = "flutter";
         };
         android = {
-          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "emulator-5554"];
           manager = "flutter";
         };
       };
