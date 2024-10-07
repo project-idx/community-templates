2ad55b7cbf18c577611f0692a89bb8e0a38650f5
Maneesh Tewani <mtewani@google.com>
2024-10-07 14:28:43 -0700

    Update firebase tools

--- dataconnect/flutter-blank/installDeps.sh
+++ dataconnect/flutter-blank/installDeps.sh
@@ -6,5 +6,6 @@ dart pub global activate flutterfire_cli
 #     firebase login --reauth && is_logged_in=true
 # done
 # Below, we update the path to remove firebase builtin.
+npm install -g firebase-tools@latest
 export PATH=/home/user/flutter/bin/:/opt/code-oss/bin/remote-cli:/home/user/.pub-cache/bin:/home/user/flutter/bin:./.flutter-sdk/flutter/bin:/usr/bin:/usr/sbin:~/.global_modules/bin:/home/user/flutter/bin:/home/user/.pub-cache/bin:/opt/android/platform-tools
 flutterfire configure -y -a com.example.blank
