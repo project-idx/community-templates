52277fb8f30441bf47125a217445b407aad3a2c6
Maneesh Tewani <mtewani@google.com>
2024-10-14 13:42:58 -0700

    Reverted firebase-cli changes

--- dataconnect/flutter-blank/installDeps.sh
+++ dataconnect/flutter-blank/installDeps.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 is_logged_in=false
 dart pub global activate flutterfire_cli
-# while [ "$is_logged_in" = false ]
-# do
-#     firebase login --reauth && is_logged_in=true
-# done
+while [ "$is_logged_in" = false ]
+do
+    firebase login --reauth && is_logged_in=true
+done
 # Below, we update the path to remove firebase builtin.
-npm install -g firebase-tools@latest
-export PATH=:~/.global_modules/bin:/home/user/flutter/bin/:/opt/code-oss/bin/remote-cli:/home/user/.pub-cache/bin:/home/user/flutter/bin:./.flutter-sdk/flutter/bin:/usr/bin:/usr/sbin:/google/idx/builtins/bin:node_modules/.bin:/home/user/flutter/bin:/home/user/.pub-cache/bin:/opt/android/platform-tools
 flutterfire configure -y -a com.example.blank
