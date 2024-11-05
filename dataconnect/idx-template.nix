b1b1cff0bddfb165a50e8e5e29d069af927fd8ca
Maneesh Tewani <mtewani@google.com>
2024-11-05 11:21:26 -0800

    Removed

--- dataconnect/idx-template.nix
+++ dataconnect/idx-template.nix
@@ -18,8 +18,7 @@ idx-template \
     if sample == "flutter-blank" || sample == "flutter-movie" then "cp -r ${./flutter}/dev.nix \"$out\"/.idx/dev.nix"
       else "cp ${./dev.nix} \"$out\"/.idx/dev.nix"
     }
-    ${
-    }
+    
     ${
       if sample == "nextjs-email-app" then "cp -r ${./nextjs-email-app}/* \"$out\""
       else if sample == "nextjs-blank" then "cp -r ${./nextjs-blank}/* \"$out\""
