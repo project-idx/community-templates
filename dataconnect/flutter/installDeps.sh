487a4d4b29a6d1ff3a8d47c71a4266239883b52d
Maneesh Tewani <mtewani@google.com>
2024-11-05 16:19:12 -0800

    Removed login

--- dataconnect/flutter/installDeps.sh
+++ dataconnect/flutter/installDeps.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 dart pub global activate flutterfire_cli
 npm install -g firebase-tools
-~/.global_modules/bin/firebase login
 # Below, we update the path to remove firebase builtin.
 flutterfire configure -y -a com.example.blank
