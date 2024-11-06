2ce8bab8868b016c79ec06dc9aa5ba8deaddd175
Maneesh Tewani <mtewani@google.com>
2024-11-05 16:45:37 -0800

    Tried to update cache

--- dataconnect/flutter/installDeps.sh
+++ dataconnect/flutter/installDeps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 npm install -g firebase-tools
 export PATH=~/.global_modules/bin:$PATH
+~/.global_modules/bin/firebase projects:list
 dart pub global activate flutterfire_cli
 # Below, we update the path to remove firebase builtin.
 flutterfire configure -y -a com.example.blank
