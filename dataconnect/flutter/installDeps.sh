8ed62c71f9e79e3b683e6f31c3a580081015fd2c
Maneesh Tewani <mtewani@google.com>
2024-11-07 15:58:24 -0800

    Updated

--- dataconnect/flutter/installDeps.sh
+++ dataconnect/flutter/installDeps.sh
@@ -4,14 +4,18 @@ export PATH=~/.global_modules/bin:$PATH
 ~/.global_modules/bin/firebase dataconnect:sdk:generate
 while [ ! -f ./.firebaserc ]
 do
-    echo "Couldn't find .firebaserc file.Please open the Data Connect extension and Connect a firebase project. Waiting 5s before checking again"
+    echo "============================================================================================================================================="
+    echo "Couldn't find .firebaserc file. Please open the Data Connect extension and Connect a firebase project. Waiting 5s before checking again"
+    echo "============================================================================================================================================="
     sleep 5s # Waits 5 seconds.
 done
 
 projectId=$(cat ./.firebaserc | grep -o '"default": "[^"]*' |  grep -o '[^"]*$')
 while [ -z "$projectId" ]
 do
+    echo "============================================================================================================================================="
     echo "Couldn't find project ID in .firebaserc. Please open the Data Connect plugin and Connect a firebase project. Waiting 5s before checking again"
+    echo "============================================================================================================================================="
     sleep 5s # Waits 5 seconds.
     projectId=$(cat ./.firebaserc | grep -o '"default": "[^"]*' |  grep -o '[^"]*$')
 done
