83a0a61e4d05ed240fe1e52112fd7dce10a516e6
Maneesh Tewani <mtewani@google.com>
2024-11-21 14:37:34 -0800

    More improvements

--- dataconnect/idx-template.nix
+++ dataconnect/idx-template.nix
@@ -13,7 +13,7 @@ idx-template \
 
   bootstrap = let  sample = 
       if platform == "web" && appType == "blank" then "nextjs-blank" else if platform == "flutter" && appType == "blank" then "flutter-blank"
-      else if platform == "flutter" && appType == "movie" then "flutter-movie"
+      else if platform == "flutter" && appType == "quickstart" then "flutter-movie"
       else "nextjs-movie-app"
    ; in ''
     mkdir "$out"
