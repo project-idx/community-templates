4de47e9113659b43d7ef3058156be35a42a2411b
Maneesh Tewani <mtewani@google.com>
2024-12-04 17:34:12 -0800

    Updated coloring

--- dataconnect/flutter-movie/lib/main.dart
+++ dataconnect/flutter-movie/lib/main.dart
@@ -152,9 +152,7 @@ class BulletList extends StatelessWidget {
                       const Text(
                         '\u2022',
                         style: TextStyle(
-                          fontSize: 16,
-                          height: 1.55,
-                        ),
+                            fontSize: 16, height: 1.55, color: Colors.white),
                       ),
                       const SizedBox(
                         width: 5,
